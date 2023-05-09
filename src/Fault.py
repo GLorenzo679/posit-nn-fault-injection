e58f6fcea077ed932a6baa673f02fae21c932250
Gabriele Lorenzo <gabriele.lorenzo6@gmail.com>
2023-05-09 12:36:42 +0200

    refactored and generalyzed fault and injection classes

--- src/Fault.py
+++ src/Fault.py
@@ -1,28 +1,41 @@
-import softposit as sp
 import sys
 
+
 class Fault:
-    def __init__(self, fault_id, layer_index, tensor_index, bit_index, bit_value):
+    def __init__(self, fault_id, layer_index, tensor_index, bit_index, type):
         self.fault_id = fault_id
         self.layer_index = layer_index
         self.tensor_index = tensor_index
         self.bit_index = bit_index
-        self.bit_value = bit_value
-        self.mask = sp.posit32()
-        self.weight_start = sp.posit32()
-        self.weight_corrupted = sp.posit32()
+        self.binary_mask = None
+        self.mask = type()
+        self.weight_start = type()
+        self.weight_corrupted = type()
 
-    def setWeightStart(self, weight_start, mask):
-        # extract binary representation of np posit32
-        np32_bin_representation = bin(int.from_bytes(weight_start.tobytes(), byteorder=sys.byteorder))
+    def set_weight(self, np_weight_start):
+        # extract binary representation of np posit
+        np_bin_representation = bin(int.from_bytes(np_weight_start.tobytes(), byteorder=sys.byteorder))
         # create a softposit posit32 with bits from np posit32
-        self.weight_start.fromBits(int(np32_bin_representation, 2))
-        self.mask.fromBits(int(mask, 0))
+        self.weight_start.fromBits(int(np_bin_representation, 2))
+
+        # print(f"np bin repr: {np_bin_representation}")
+        # print(f"weight start: {self.weight_start}")
+
+        self.mask.fromBits(int(self.binary_mask, 0))
+
+        print(f"binary_mask: {self.binary_mask}")
 
         self.weight_corrupted = self.weight_start ^ self.mask
-        
-    def printFault(self):
-        print(str(self.fault_id) + ", " + str(self.layer_index) + ", " +
-                str(self.tensor_index) + ", " + str(self.bit_index) + ", " + str(self.bit_value))
-        
 
+    # def print_fault(self):
+    #    print(
+    #        str(self.fault_id)
+    #        + ", "
+    #        + str(self.layer_index)
+    #        + ", "
+    #        + str(self.tensor_index)
+    #        + ", "
+    #        + str(self.bit_index)
+    #        + ", "
+    #        + str(self.bit_value)
+    #    )
--- src/Injection.py
+++ src/Injection.py
@@ -1,35 +1,56 @@
-from scipy.stats import norm
 import random
-from Fault import Fault
+
+from scipy.stats import norm
+from src.Fault import Fault
 
 
 class Injection:
     def __init__(self):
         self.fault_list = []
+        self.type = None
+        self.num_bit_representation = None
 
-    def createInjectionList(self, num_weight_net, num_bit_representation, num_layer, num_batch, batch_height, batch_width, batch_features):
+    def __compute_binary_mask__(self, fault):
+        return "0b" + "0" * (self.num_bit_representation - 1 - fault.bit_index) + str(1) + "0" * (fault.bit_index)
+
+    def createInjectionList(
+        self,
+        num_weight_net,
+        num_bit_representation,
+        num_layer,
+        num_batch,
+        batch_height,
+        batch_width,
+        batch_features,
+        type,
+    ):
         self.num_bit_representation = num_bit_representation
+        self.type = type
+
         t = compute_t(0.8)
-        N  = num_weight_net * num_bit_representation * 2
+        N = num_weight_net * num_bit_representation * 2
+
         number_of_faults = int(compute_date_n(N, 0.5, 0.01, t))
         print(f"Number of faults to apply: {number_of_faults}")
 
         for i in range(number_of_faults):
             fault_id = i
             layer_index = random.randrange(0, num_layer)
-            tensor_index = (random.randrange(0, num_batch),
-                            random.randrange(0, batch_height),
-                            random.randrange(0, batch_width),
-                            random.randrange(0,batch_features))
+            tensor_index = (
+                random.randrange(0, num_batch),
+                random.randrange(0, batch_height),
+                random.randrange(0, batch_width),
+                random.randrange(0, batch_features),
+            )
             bit_index = random.randrange(0, num_bit_representation)
-            bit_value = 1
-            fault = Fault(fault_id, layer_index, tensor_index, bit_index, bit_value)
-            self.fault_list.append(fault)
 
+            fault = Fault(fault_id, layer_index, tensor_index, bit_index, self.type)
+            fault.binary_mask = self.__compute_binary_mask__(fault)
+            self.fault_list.append(fault)
 
     def printInjectionList(self):
         for i in range(len(self.fault_list)):
-            self.fault_list[i].printFault()    
+            self.fault_list[i].printFault()
 
 
 def compute_t(confidence_level: float = 0.8):
@@ -37,16 +58,13 @@ def compute_t(confidence_level: float = 0.8):
     Compute the t value from the confidence level
     :param confidence level: The desired confidence level
     :return: The t corresponding to the input confidence level
-    """   
+    """
     alpha = (1 - confidence_level) / 2
     t = -norm.ppf(alpha)
     return t
 
 
-def compute_date_n(N: int,
-                     p: float = 0.5,
-                     e: float = 0.01,
-                     t: float = 2.58):
+def compute_date_n(N: int, p: float = 0.5, e: float = 0.01, t: float = 2.58):
     """
     Compute the number of faults to inject according to the DATE23 formula
     :param N: The total number of parameters. If None, compute the infinite population version
@@ -56,11 +74,6 @@ def compute_date_n(N: int,
     :return: the number of fault to inject
     """
     if N is None:
-        return p * (1-p) * t ** 2 / e ** 2
+        return p * (1 - p) * t**2 / e**2
     else:
-        return N / (1 + e ** 2 * (N - 1) / (t ** 2 * p * (1 - p)))
-
-
-
-
-
+        return N / (1 + e**2 * (N - 1) / (t**2 * p * (1 - p)))
