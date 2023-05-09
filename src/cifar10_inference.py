07b649638954d9c2792dfc5b14b6e747b0057b08
Gabriele Lorenzo <gabriele.lorenzo6@gmail.com>
2023-05-09 20:51:21 +0200

    cleaned inference file

--- src/cifar10_inference.py
+++ src/cifar10_inference.py
@@ -1,48 +1,40 @@
 from __future__ import division, print_function
 
 import os
-import sys
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets import cifar10
 
 
 class Inference:
-    def __init__(self, data_t, model, evaluator):
+    def __init__(self, data_t, nn_model, evaluator, batch_size, data_set_size, data_set, loader):
         np.random.seed(1)
         tf.set_random_seed(2)
 
-        self.data_t = data_t
-        self.model = model
         self.evaluator = evaluator
 
-        # Load CIFAR10 Dataset
-        data_set = "CIFAR-10"
+        # load dataset
         print("Dataset is:", data_set)
-        (_, _), (X_test, self.y_test) = cifar10.load_data()
+        (_, _), (X_test, y_test) = loader()
         # somehow y_train comes as a 2D nx1 matrix
-        self.y_test = self.y_test.reshape(self.y_test.shape[0])
+        self.y_test = y_test.reshape(y_test.shape[0])
 
         assert len(X_test) == len(self.y_test)
 
-        self.test_size = 128
-        # rand_index = random.randrange(0, len(X_test) - test_size)
-        rand_index = np.random.randint(0, len(X_test) - self.test_size)
-        X_test = X_test[rand_index : rand_index + self.test_size]
-        self.y_test = self.y_test[rand_index : rand_index + self.test_size]
+        # take a random subset of test samples of dimension = data_set_size
+        rand_index = np.random.randint(0, len(X_test) - data_set_size)
+        X_test = X_test[rand_index : rand_index + data_set_size]
+        self.y_test = self.y_test[rand_index : rand_index + data_set_size]
 
         print("\nImage Shape: {}\n".format(X_test[0].shape))
         print("Test Set: {} samples".format(len(X_test)))
 
-        """## Setup TensorFlow
-        The `EPOCH` and `BATCH_SIZE` values affect the training speed and model accuracy.
-        """
+        # size of batches to process
+        # num_batches =  data_set_size / batch_size
+        self.batch_size = batch_size
+        print("Batch size:", self.batch_size)
 
-        self.BATCH_SIZE = 128
-        print("Batch size:", self.BATCH_SIZE)
-
-        # Set Posit data types
+        # Set data type constructors to use
         if data_t == "posit32":
             self.posit = np.posit32
             self.tf_type = tf.posit32
@@ -55,13 +47,11 @@ class Inference:
         elif data_t == "float32":
             self.posit = np.float32
             self.tf_type = tf.float32
-        else:
-            sys.exit("Incorrect data type provided in args")
 
-        # confirm dtype
-        print("\nType is:", self.data_t)
+        # confirm data_t
+        print("\nType is:", data_t)
 
-        # Normalize data
+        # Normalize data and convert it to selected data_t
         self.X_test = ((X_test - 127.5) / 127.5).astype(self.posit)
 
         print("Input data type: {}".format(type(self.X_test[0, 0, 0, 0])))
@@ -71,16 +61,14 @@ class Inference:
         `x` is a placeholder for a batch of input images.
         `y` is a placeholder for a batch of output labels.
         """
-
         self.x = tf.placeholder(self.tf_type, (None, 32, 32, 3))
         self.y = tf.placeholder(tf.int32, (None))
         self.training = tf.placeholder(tf.bool)
 
-        """## Model Evaluation
-        Evaluate how well the loss and accuracy of the model for a given dataset.
-        """
+        # MAYBE WE CAN MOVE THIS NEXT PART
 
-        logits = self.model(self.x, self.training, self.tf_type)
+        # setup operations to evaluate the model (prediction accuracy and top-5)
+        logits = nn_model(self.x, self.tf_type)
 
         correct_prediction = tf.equal(tf.argmax(logits, 1, output_type=tf.int32), self.y)
         self.accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -95,34 +83,11 @@ class Inference:
         """
         tf.graph_util.remove_training_nodes(tf.get_default_graph().as_graph_def())
 
-        self.path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-        self.data_dir = self.path + "/data/CIFAR10/"
-        self.model_name = self.data_dir + data_t + ".ckpt"
+        path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        data_dir = path + "/data/" + data_set + "/"
+        self.model_name = data_dir + data_t + ".ckpt"
 
-        assert os.path.exists(self.data_dir), "The directory %s does not exist!" % self.data_dir
-
-    def evaluate(self, X_data, y_data):
-        num_examples = len(X_data)
-        total_accuracy = 0
-        total_top5 = 0
-        sess = tf.get_default_session()
-
-        for offset in range(0, num_examples, self.BATCH_SIZE):
-            print(f"\nBatch {int(offset/self.BATCH_SIZE + 1)}:")
-            batch_x, batch_y = (
-                X_data[offset : offset + self.BATCH_SIZE],
-                y_data[offset : offset + self.BATCH_SIZE],
-            )
-            accuracy, top5 = sess.run(
-                [self.accuracy_operation, self.top5_operation],
-                feed_dict={self.x: batch_x, self.y: batch_y, self.training: False},
-            )
-
-            print(f"\tAccuracy: {accuracy}\tTop-5: {top5}")
-
-            total_accuracy += accuracy * len(batch_x)
-            total_top5 += top5 * len(batch_x)
-        return (total_accuracy / num_examples, total_top5 / num_examples)
+        assert os.path.exists(data_dir), "The directory %s does not exist!" % data_dir
 
     def compute_inference(self, fault=None):
         """## Load and cast the Pre-trained Model before Evaluate
@@ -139,7 +104,9 @@ class Inference:
             for variable in tf.global_variables():
                 if variable.op.name in previous_variables:
                     var = tf.contrib.framework.load_variable(self.model_name, variable.op.name)
-                    if var.dtype == np.posit32:
+                    # this is not ok --> need to change in order to generalize data type
+                    # if var.dtype == np.posit32:
+                    if var.dtype == self.posit:
                         tf.add_to_collection("assignOps", variable.assign(tf.cast(var, self.tf_type)))
                     else:
                         print("Var. found of type ", var.dtype)
@@ -164,16 +131,24 @@ class Inference:
 
             print("Computing Acc. (Top-1) & Top-5...")
 
-            # val_acc, test_top5 = self.evaluator(self.X_test, self.y_test, self.BATCH_SIZE, self.x, self.y, self.training, self.accuracy_operation, self.top5_operation)
-
-            val_acc, test_top5 = self.evaluate(self.X_test, self.y_test)
+            # perform evaluation on model
+            val_acc, test_top5 = self.evaluator(
+                self.X_test,
+                self.y_test,
+                self.batch_size,
+                self.x,
+                self.y,
+                self.training,
+                self.accuracy_operation,
+                self.top5_operation,
+            )
 
             # reset injected weight
             if fault != None:
                 np_weights[fault.tensor_index] = fault.weight_start
-                sess.run(tf.assign(weights, tf.convert_to_tensor(np_weights, dtype=np.posit32)))
+                sess.run(tf.assign(weights, tf.convert_to_tensor(np_weights, dtype=self.posit)))
 
-            print("Accuracy:", val_acc)
-            print("Top-5:", test_top5)
+            print("Mean accuracy:", val_acc)
+            print("Mean top-5:", test_top5)
 
         return val_acc, test_top5
